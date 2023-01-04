4f45387d018f0459540cb15148d2f871dccb5c68
phasetr <phasetr@gmail.com>
2023-01-04 10:53:47 +0900

    update

--- MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
 using MvcMovie.Models;
@@ -15,14 +16,26 @@ public class MoviesController : Controller
     }
 
     // GET: Movies
-    public async Task<IActionResult> Index(string searchString)
+    public async Task<IActionResult> Index(string movieGenre, string searchString)
     {
+        // Use LINQ to get list of genres.
+        IQueryable<string> genreQuery = from m in _context.Movie
+            orderby m.Genre
+            select m.Genre;
         var movies = from m in _context.Movie
             select m;
 
         if (!string.IsNullOrEmpty(searchString)) movies = movies.Where(s => s.Title!.Contains(searchString));
 
-        return View(await movies.ToListAsync());
+        if (!string.IsNullOrEmpty(movieGenre)) movies = movies.Where(x => x.Genre == movieGenre);
+
+        var movieGenreVm = new MovieGenreViewModel
+        {
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+            Movies = await movies.ToListAsync()
+        };
+
+        return View(movieGenreVm);
     }
 
     // GET: Movies/Details/5
