d40030994d133aa345004196e44a1cbc3af63a77
phasetr <phasetr@gmail.com>
2023-01-27 09:52:43 +0900

    update

--- MainSample/MainSample/Program.cs
+++ MainSample/MainSample/Program.cs
@@ -1,18 +1,26 @@
 using MainSample.Data;
+using MainSample.Filters;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var logger = LoggerFactory.Create(config =>
+{
+    config.AddConsole();
+    config.SetMinimumLevel(LogLevel.Trace);
+}).CreateLogger("MyAsyncPageFilter Sample");
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddMvcOptions(options =>
+{
+    options.Filters.Add(new MyAsyncPageFilter(logger));
+});
 
 var app = builder.Build();
 
