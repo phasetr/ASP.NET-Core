d4b6fd49716e220b5b4befc4892a73185fe9cabc
phasetr <phasetr@gmail.com>
2023-04-16 09:43:00 +0900

    シードのユーザー追加。

--- JwtSample/WebApi/Data/Configurations/ApplicationUserConfiguration.cs
+++ JwtSample/WebApi/Data/Configurations/ApplicationUserConfiguration.cs
@@ -11,6 +11,7 @@ public class ApplicationUserConfiguration : IEntityTypeConfiguration<Application
 
     // System.Guid.NewGuid().ToString()
     private static readonly Guid Admin1Guid = Guid.Parse("9145cd60-8280-428e-a399-abfbfe7db5a9");
+    private static readonly Guid Admin2Guid = Guid.Parse("27a80c31-8582-4f28-a3ff-3bb0011def73");
     private static readonly PasswordHasher<string> Hasher = new();
 
     public void Configure(EntityTypeBuilder<ApplicationUser> builder)
@@ -25,6 +26,19 @@ public class ApplicationUserConfiguration : IEntityTypeConfiguration<Application
             NormalizedUserName = "test".ToUpper(),
             Email = "test@phasetr.com",
             NormalizedEmail = "test@phasetr.com".ToUpper(),
+            EmailConfirmed = true,
+            SecurityStamp = Admin1Guid.ToString(),
+            ConcurrencyStamp = Admin1Guid.ToString(),
+            PasswordHash = Hasher.HashPassword(HashString, "testTest")
+        }, new ApplicationUser
+        {
+            Id = Admin2Guid.ToString(),
+            UserName = "test2",
+            FirstName = "test2 first",
+            LastName = "test2 last",
+            NormalizedUserName = "test2".ToUpper(),
+            Email = "test2@phasetr.com",
+            NormalizedEmail = "test2@phasetr.com".ToUpper(),
             EmailConfirmed = true,
             SecurityStamp = Admin1Guid.ToString(),
             ConcurrencyStamp = Admin1Guid.ToString(),
