8a1c69e689554b1397eece675310cd8caeee3743
phasetr <phasetr@gmail.com>
2023-04-29 00:39:17 +0900

    いったんコミット。

--- BlazorWasmHosted/Client/Program.cs
+++ BlazorWasmHosted/Client/Program.cs
@@ -10,12 +10,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(_ =>
     new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddScoped<AuthenticationHttpClient>();
 builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
     provider.GetRequiredService<CustomAuthenticationStateProvider>());
+
 await builder.Build().RunAsync();
