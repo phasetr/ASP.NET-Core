8606a1eaef2b87a77b80303570d02122c7863d8a
phasetr <phasetr@gmail.com>
2023-08-24 14:37:09 +0900

    commit

--- BlazorJwtAuth/BlazorJwtAuth.WebApi.Service/Services/IUserService.cs
+++ BlazorJwtAuth/BlazorJwtAuth.WebApi.Service/Services/IUserService.cs
@@ -8,9 +8,7 @@ public interface IUserService
     Task<string> RegisterAsync(RegisterModel model);
     Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
     Task<string> AddRoleAsync(AddRoleModel model);
-
     Task<AuthenticationModel> RefreshTokenAsync(string jwtToken);
-
     Task<bool> RevokeTokenAsync(string token);
     Task<ApplicationUser?> GetByIdAsync(string id);
 }
