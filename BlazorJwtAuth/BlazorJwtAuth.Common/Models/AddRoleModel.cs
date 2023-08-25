87495866d0a2747277102e294084c7595627ce22
phasetr <phasetr@gmail.com>
2023-08-25 13:21:54 +0900

    commit

--- BlazorJwtAuth/BlazorJwtAuth.Common/Models/AddRoleModel.cs
+++ BlazorJwtAuth/BlazorJwtAuth.Common/Models/AddRoleModel.cs
@@ -4,9 +4,9 @@ namespace BlazorJwtAuth.Common.Models;
 
 public class AddRoleModel
 {
-    [Required] public string Email { get; set; }
+    [Required] public string Email { get; set; } = default!;
 
-    [Required] public string Password { get; set; }
+    [Required] public string Password { get; set; } = default!;
 
-    [Required] public string Role { get; set; }
+    [Required] public string Role { get; set; } = default!;
 }
--- BlazorJwtAuth/BlazorJwtAuth.Common/Models/RegisterModel.cs
+++ BlazorJwtAuth/BlazorJwtAuth.Common/Models/RegisterModel.cs
@@ -4,13 +4,13 @@ namespace BlazorJwtAuth.Common.Models;
 
 public class RegisterModel
 {
-    [Required] public string FirstName { get; set; }
+    [Required] public string FirstName { get; set; } = default!;
 
-    [Required] public string LastName { get; set; }
+    [Required] public string LastName { get; set; } = default!;
 
-    [Required] public string Username { get; set; }
+    [Required] public string Username { get; set; } = default!;
 
-    [Required] public string Email { get; set; }
+    [Required] public string Email { get; set; } = default!;
 
-    [Required] public string Password { get; set; }
+    [Required] public string Password { get; set; } = default!;
 }
