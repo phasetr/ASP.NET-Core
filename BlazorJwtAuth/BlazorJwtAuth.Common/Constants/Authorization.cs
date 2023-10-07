b5fca90d2e0452c777247fa7c7e2e88ebd7be4b6
phasetr <phasetr@gmail.com>
2023-10-07 23:25:33 +0900

    commit

--- BlazorJwtAuth/BlazorJwtAuth.Common/Constants/Authorization.cs
+++ BlazorJwtAuth/BlazorJwtAuth.Common/Constants/Authorization.cs
@@ -14,5 +14,10 @@ public class Authorization
     public const string DefaultPassword = "Pa$$w0rd.";
     public const Roles DefaultRole = Roles.User;
 
+    /// <summary>
+    ///     JWTのアクセストークン名。
+    ///     アクセストークンはCookieに保存される。
+    ///     時間設定は`WebApi`の`Program.cs`で直接設定している。
+    /// </summary>
     public const string JwtAccessTokenName = "accessToken";
 }
