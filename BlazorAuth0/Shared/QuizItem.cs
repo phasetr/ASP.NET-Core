2d93180c32ae98e4a78c3e5316c639d13bbed62d
phasetr <phasetr@gmail.com>
2023-10-19 15:25:28 +0900

    delete

--- BlazorAuth0/Shared/QuizItem.cs
+++
@@ -1,9 +0,0 @@
-namespace QuizManagerClientHosted.Shared;
-
-public class QuizItem
-{
-    public string Question { get; set; } = default!;
-    public List<string> Choices { get; set; } = default!;
-    public int AnswerIndex { get; set; }
-    public int Score { get; set; }
-}
