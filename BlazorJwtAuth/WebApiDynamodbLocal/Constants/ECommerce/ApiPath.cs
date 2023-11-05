92c158b7381786f65ee3e9e5ca37f46f74a317b5
phasetr <phasetr@gmail.com>
2023-11-05 17:08:37 +0900

    commit

--- BlazorJwtAuth/WebApiDynamodbLocal/Constants/ECommerce/ApiPath.cs
+++ BlazorJwtAuth/WebApiDynamodbLocal/Constants/ECommerce/ApiPath.cs
@@ -2,6 +2,6 @@ namespace WebApiDynamodbLocal.Constants.ECommerce;
 
 public static class ApiPath
 {
-    public const string Customer = "api/customer";
-    public const string Order = "api/order";
+    public const string Customer = "api/ecommerce/customer";
+    public const string Order = "api/ecommerce/order";
 }
