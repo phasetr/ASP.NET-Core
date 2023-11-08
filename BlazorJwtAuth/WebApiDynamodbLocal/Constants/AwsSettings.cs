f74f37ec17db4fc8f4fbcf36cd6016389abfe5fb
phasetr <phasetr@gmail.com>
2023-11-08 21:11:54 +0900

    commit

--- BlazorJwtAuth/WebApiDynamodbLocal/Constants/AwsSettings.cs
+++ BlazorJwtAuth/WebApiDynamodbLocal/Constants/AwsSettings.cs
@@ -2,7 +2,6 @@ namespace WebApiDynamodbLocal.Constants;
 
 public static class AwsSettings
 {
-    public const string ECommerceTable = "ECommerce";
     public const string ConfigurationECommerceTable = "Dynamodb:ECommerceTableName";
     public const string ConfigurationSessionStoreTable = "Dynamodb:SessionStoreTableName";
     public const string ConfigurationBigTimeDealsTable = "Dynamodb:BigTimeDealsTableName";
