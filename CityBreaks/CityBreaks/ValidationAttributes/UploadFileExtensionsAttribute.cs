79f1cfcf26883470b221c40e928e8de49e0d26fc
phasetr <phasetr@gmail.com>
2023-11-10 10:48:04 +0900

    commit

--- CityBreaks/CityBreaks/ValidationAttributes/UploadFileExtensionsAttribute.cs
+++ CityBreaks/CityBreaks/ValidationAttributes/UploadFileExtensionsAttribute.cs
@@ -4,19 +4,19 @@ namespace CityBreaks.ValidationAttributes;
 
 public class UploadFileExtensionsAttribute : ValidationAttribute
 {
-    private IEnumerable<string> allowedExtensions;
+    private IEnumerable<string> _allowedExtensions;
     public string Extensions { get; set; }
 
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        allowedExtensions = Extensions?
+        _allowedExtensions = Extensions?
             .Split(new[] {','},
                 StringSplitOptions.RemoveEmptyEntries)
             .Select(x => x.ToLowerInvariant());
-        if (value is IFormFile file && allowedExtensions.Any())
+        if (value is IFormFile file && _allowedExtensions.Any())
         {
             var extension = Path.GetExtension(file.FileName.ToLowerInvariant());
-            if (!allowedExtensions.Contains(extension))
+            if (!_allowedExtensions.Contains(extension))
                 return new ValidationResult(ErrorMessage
                                             ?? $"The file extension must be {Extensions}");
         }
