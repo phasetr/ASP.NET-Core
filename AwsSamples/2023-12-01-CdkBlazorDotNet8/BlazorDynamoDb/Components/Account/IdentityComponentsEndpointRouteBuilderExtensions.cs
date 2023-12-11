44294fbc020f843ff723ca3582190936b48ac6fd
phasetr <phasetr@gmail.com>
2023-12-11 09:46:47 +0900

    commit

--- AwsSamples/2023-12-01-CdkBlazorDotNet8/BlazorDynamoDb/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ AwsSamples/2023-12-01-CdkBlazorDotNet8/BlazorDynamoDb/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -29,8 +29,8 @@ internal static class IdentityComponentsEndpointRouteBuilderExtensions
         {
             IEnumerable<KeyValuePair<string, StringValues>> query =
             [
-                new("ReturnUrl", returnUrl),
-                new("Action", ExternalLogin.LoginCallbackAction)
+                new KeyValuePair<string, StringValues>("ReturnUrl", returnUrl),
+                new KeyValuePair<string, StringValues>("Action", ExternalLogin.LoginCallbackAction)
             ];
 
             var redirectUrl = UriHelper.BuildRelative(
@@ -81,33 +81,25 @@ internal static class IdentityComponentsEndpointRouteBuilderExtensions
         {
             var user = await userManager.GetUserAsync(context.User);
             if (user is null)
-            {
                 return Results.NotFound($"Unable to load user with ID '{userManager.GetUserId(context.User)}'.");
-            }
 
             var userId = await userManager.GetUserIdAsync(user);
             downloadLogger.LogInformation("User with ID '{UserId}' asked for their personal data", userId);
 
             // Only include personal data for download
-            var personalData = new Dictionary<string, string>();
             var personalDataProps = typeof(ApplicationUser).GetProperties().Where(
                 prop => Attribute.IsDefined(prop, typeof(PersonalDataAttribute)));
-            foreach (var p in personalDataProps)
-            {
-                personalData.Add(p.Name, p.GetValue(user)?.ToString() ?? "null");
-            }
+            var personalData = personalDataProps
+                .ToDictionary(p => p.Name, p => p.GetValue(user)?.ToString() ?? "null");
 
             var logins = await userManager.GetLoginsAsync(user);
-            foreach (var l in logins)
-            {
-                personalData.Add($"{l.LoginProvider} external login provider key", l.ProviderKey);
-            }
+            foreach (var l in logins) personalData.Add($"{l.LoginProvider} external login provider key", l.ProviderKey);
 
             personalData.Add("Authenticator Key", (await userManager.GetAuthenticatorKeyAsync(user))!);
             var fileBytes = JsonSerializer.SerializeToUtf8Bytes(personalData);
 
             context.Response.Headers.TryAdd("Content-Disposition", "attachment; filename=PersonalData.json");
-            return TypedResults.File(fileBytes, contentType: "application/json", fileDownloadName: "PersonalData.json");
+            return TypedResults.File(fileBytes, "application/json", "PersonalData.json");
         });
 
         return accountGroup;
