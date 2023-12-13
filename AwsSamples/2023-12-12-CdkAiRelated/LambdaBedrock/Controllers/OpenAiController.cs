5d3ab96b3b00f75e67605a495309e81133175d13
phasetr <phasetr@gmail.com>
2023-12-13 13:37:21 +0900

    commit

---
+++ AwsSamples/2023-12-12-CdkAiRelated/LambdaBedrock/Controllers/OpenAiController.cs
@@ -0,0 +1,66 @@
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using LambdaBedrock.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Chat;
+
+namespace LambdaBedrock.Controllers;
+
+[ApiController]
+[Route("openai")]
+public class OpenAiController : ControllerBase
+{
+    private readonly ILogger<OpenAiController> _logger;
+
+    public OpenAiController(ILogger<OpenAiController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ResponseBaseDto> GetAsync()
+    {
+        _logger.LogInformation("OpenAiController.GetAsync");
+
+        var client = new AmazonSimpleSystemsManagementClient();
+        var request = new GetParameterRequest
+        {
+            Name = "OPENAI_API_KEY",
+            WithDecryption = true
+        };
+
+        try
+        {
+            var response = await client.GetParameterAsync(request);
+            var apiKey = response.Parameter.Value;
+            var api = new OpenAIAPI(apiKey);
+            var message = new ChatMessage
+            {
+                Role = ChatMessageRole.User,
+                TextContent = "富士山の高さは何メートルですか？"
+            };
+            var result = await api.Chat.CreateChatCompletionAsync(
+                model: "gpt-3.5-turbo",
+                messages: new List<ChatMessage> {message});
+            var textContent = result.Choices[0].Message.TextContent;
+            _logger.LogInformation("{T}", textContent);
+
+            return new ResponseBaseDto
+            {
+                Message = textContent,
+                Succeeded = true
+            };
+        }
+        catch (AmazonSimpleSystemsManagementException e)
+        {
+            _logger.LogError("{E}", e.Message);
+            _logger.LogError("{E}", e.StackTrace);
+            return new ResponseBaseDto
+            {
+                Message = e.Message,
+                Succeeded = false
+            };
+        }
+    }
+}
