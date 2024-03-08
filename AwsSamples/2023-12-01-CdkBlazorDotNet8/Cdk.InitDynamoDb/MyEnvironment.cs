90ff3f65ff4fec526d6a03bad14a8e7de38ab77f
phasetr <phasetr@gmail.com>
2024-03-08 17:01:54 +0900

    commit

---
+++ AwsSamples/2023-12-01-CdkBlazorDotNet8/Cdk.InitDynamoDb/MyEnvironment.cs
@@ -0,0 +1,7 @@
+namespace Cdk.InitDynamoDb;
+
+public enum MyEnvironment
+{
+    Local = 0,
+    Dev = 1
+}
--- AwsSamples/2023-12-01-CdkBlazorDotNet8/Cdk.InitDynamoDb/Program.cs
+++ AwsSamples/2023-12-01-CdkBlazorDotNet8/Cdk.InitDynamoDb/Program.cs
@@ -1,28 +1,48 @@
 using Amazon.DynamoDBv2;
 using AspNetCore.Identity.AmazonDynamoDB;
 using Cdk.Common;
+using Cdk.InitDynamoDb;
 using Microsoft.Extensions.DependencyInjection;
 
-const string serviceUrl = Constants.DynamoDbLocalUrl;
+InitializeDynamoDb(MyEnvironment.Local);
+InitializeDynamoDb(MyEnvironment.Dev);
+return;
 
-var services = new ServiceCollection();
-var client = new AmazonDynamoDBClient(
-    string.IsNullOrEmpty(serviceUrl) == false
-        ? new AmazonDynamoDBConfig {ServiceURL = serviceUrl}
-        : new AmazonDynamoDBConfig());
+void InitializeDynamoDb(MyEnvironment env)
+{
+    var services = new ServiceCollection();
+    var amazonDynamoDbConfig = env switch
+    {
+        MyEnvironment.Local => new AmazonDynamoDBConfig {ServiceURL = Constants.DynamoDbLocalUrl},
+        MyEnvironment.Dev => new AmazonDynamoDBConfig(),
+        _ => throw new ArgumentOutOfRangeException(nameof(env), env, null)
+    };
+    var client = new AmazonDynamoDBClient(amazonDynamoDbConfig);
 
-services.AddIdentityCore<DynamoDbUser>()
-    .AddRoles<DynamoDbRole>()
-    .AddDynamoDbStores().Configure(options => { options.DefaultTableName = Constants.DynamoDbLocalTableName; });
-services.AddSingleton<IAmazonDynamoDB>(client);
+    services.AddIdentityCore<DynamoDbUser>()
+        .AddRoles<DynamoDbRole>()
+        .AddDynamoDbStores().Configure(options =>
+        {
+            options.DefaultTableName = env switch
+            {
+                MyEnvironment.Local => Constants.DynamoDbLocalTableName,
+                MyEnvironment.Dev => Constants.DynamoDbTableName,
+                _ => throw new ArgumentOutOfRangeException(nameof(env), env, null)
+            };
+        });
+    services.AddSingleton<IAmazonDynamoDB>(client);
 
-var serviceProvider = services.BuildServiceProvider();
-// Ensure Tables Is Created
-AspNetCoreIdentityDynamoDbSetup.EnsureInitialized(serviceProvider);
-var tables = client.ListTablesAsync().GetAwaiter().GetResult();
-Console.WriteLine("Tables initialized, the following tables exists:");
-tables.TableNames.ForEach(Console.WriteLine);
+    var serviceProvider = services.BuildServiceProvider();
+    // Ensure Tables Is Created
+    AspNetCoreIdentityDynamoDbSetup.EnsureInitialized(serviceProvider);
+    var tables = client.ListTablesAsync().GetAwaiter().GetResult();
+    Console.WriteLine($"NOW {env}");
+    Console.WriteLine("Tables initialized, the following tables exists:");
+    tables.TableNames.ForEach(Console.WriteLine);
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("All good!");
-Console.ResetColor();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("All good!");
+    Console.ResetColor();
+    Console.WriteLine();
+    Console.WriteLine();
+}
