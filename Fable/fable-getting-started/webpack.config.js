550fcec92142a9d7ed3cefb5efdb7c77328ed3e0
phasetr <phasetr@gmail.com>
2024-04-10 21:30:36 +0900

    commit

--- Fable/fable-getting-started/webpack.config.js
+++ Fable/fable-getting-started/webpack.config.js
@@ -1,13 +1,15 @@
 var path = require("path");
 
 module.exports = {
-    mode: "none",
-    entry: "./src/App.fs.js",
-    output: {
-        path: path.join(__dirname, "./dist"),
-        filename: "main.js"
-    },
-    devServer: {
-        static: path.join(__dirname, "./dist")
+  mode: "development",
+  entry: "./src/App.fs.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js"
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist")
     }
+  }
 }
