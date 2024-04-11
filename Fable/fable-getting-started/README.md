71e02cd278dd3db6fbd552c8a6bf61fa0960bf39
phasetr <phasetr@gmail.com>
2024-04-11 09:19:59 +0900

    commit

--- Fable/fable-getting-started/README.md
+++ Fable/fable-getting-started/README.md
@@ -2,6 +2,12 @@
 
 - [Original Repository](https://github.com/Zaid-Ajaj/fable-getting-started)
 
+### 開発用に`watch`で実行
+
+```shell
+npm start
+```
+
 ### 単純な実行
 
 - 以下のコマンドを実行して`dist/index.html`をブラウザで開く
--- Fable/fable-getting-started/src/App.fs
+++ Fable/fable-getting-started/src/App.fs
@@ -1,7 +1,42 @@
 module App
+
 open Browser.Dom
 
 printfn "Hello from Fable"
+
+let increaseButton = document.getElementById "increase"
+let decreaseButton = document.getElementById "decrease"
+let countViewer = document.getElementById "countViewer"
+
+let mutable currentCount = 0
+let rnd = System.Random()
+
+let runAfter (ms: int) callback =
+  async {
+    do! Async.Sleep ms
+    do callback ()
+  }
+  |> Async.StartImmediate
+
+let increaseDelayed = document.getElementById "increaseDelayed"
+
+increaseButton.onclick <-
+  fun _ ->
+    currentCount <- currentCount + rnd.Next(5, 10)
+    countViewer.innerHTML <- $"Count is at %d{currentCount}"
+
+decreaseButton.onclick <-
+  fun _ ->
+    currentCount <- currentCount - rnd.Next(5, 10)
+    countViewer.innerHTML <- $"Count is at %d{currentCount}"
+
+increaseDelayed.onclick <-
+  fun _ ->
+    runAfter 1000 (fun () ->
+      currentCount <- currentCount + rnd.Next(5, 10)
+      countViewer.innerHTML <- $"Count is at %d{currentCount}")
+
+countViewer.innerHTML <- $"Count is at %d{currentCount}"
 
 let printMsgButton = document.getElementById "printMsg"
 
