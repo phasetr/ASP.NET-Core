f6ef8f600ffcbfdcfb46d01a6906bc0d04de2ba9
phasetr <phasetr@gmail.com>
2024-04-18 17:49:29 +0900

    commit

--- Fable/ToDoApp/src/App.fs
+++ Fable/ToDoApp/src/App.fs
@@ -9,14 +9,22 @@ type Todo =
     Description: string
     Completed: bool }
 
+type TodoBeingEdited = { Id: int; Description: string }
+
 type State =
-  { TodoList: Todo list; NewTodo: string }
+  { TodoList: Todo list
+    NewTodo: string
+    TodoBeingEdited: TodoBeingEdited option }
 
 type Msg =
   | SetNewTodo of string
   | AddNewTodo
   | DeleteTodo of int
   | ToggleCompleted of int
+  | CancelEdit
+  | ApplyEdit
+  | StartEditingTodo of int
+  | SetEditedDescription of string
 
 let init () =
   { TodoList =
@@ -26,14 +34,13 @@ let init () =
         { Id = 2
           Description = "Learn Elmish"
           Completed = false } ]
-    NewTodo = "" }
+    NewTodo = ""
+    TodoBeingEdited = None }
 
 let update (msg: Msg) (state: State) =
   match msg with
   | SetNewTodo desc -> { state with NewTodo = desc }
-
   | AddNewTodo when state.NewTodo = "" -> state
-
   | AddNewTodo ->
     let nextTodoId =
       match state.TodoList with
@@ -48,12 +55,9 @@ let update (msg: Msg) (state: State) =
     { state with
         NewTodo = ""
         TodoList = List.append state.TodoList [ nextTodo ] }
-
   | DeleteTodo todoId ->
     let nextTodoList = state.TodoList |> List.filter (fun todo -> todo.Id <> todoId)
-
     { state with TodoList = nextTodoList }
-
   | ToggleCompleted todoId ->
     let nextTodoList =
       state.TodoList
@@ -65,6 +69,43 @@ let update (msg: Msg) (state: State) =
           todo)
 
     { state with TodoList = nextTodoList }
+  | StartEditingTodo todoId ->
+    let nextEditModel =
+      state.TodoList
+      |> List.tryFind (fun todo -> todo.Id = todoId)
+      |> Option.map (fun todo ->
+        { Id = todoId
+          Description = todo.Description })
+
+    { state with
+        TodoBeingEdited = nextEditModel }
+  | CancelEdit -> { state with TodoBeingEdited = None }
+  | ApplyEdit ->
+    match state.TodoBeingEdited with
+    | None -> state
+    | Some todoBeingEdited when todoBeingEdited.Description = "" -> state
+    | Some todoBeingEdited ->
+      let nextTodoList =
+        state.TodoList
+        |> List.map (fun todo ->
+          if todo.Id = todoBeingEdited.Id then
+            { todo with
+                Description = todoBeingEdited.Description }
+          else
+            todo)
+
+      { state with
+          TodoList = nextTodoList
+          TodoBeingEdited = None }
+  | SetEditedDescription newText ->
+    let nextEditModel =
+      state.TodoBeingEdited
+      |> Option.map (fun todoBeingEdited ->
+        { todoBeingEdited with
+            Description = newText })
+
+    { state with
+        TodoBeingEdited = nextEditModel }
 
 // Helper function to easily construct div with only classes and children
 let div (classes: string list) (children: ReactElement list) =
@@ -89,6 +130,30 @@ let inputField (state: State) (dispatch: Msg -> unit) =
               prop.onClick (fun _ -> dispatch AddNewTodo)
               prop.children [ Html.i [ prop.classes [ "fa"; "fa-plus" ] ] ] ] ] ]
 
+let renderEditForm (todoBeingEdited: TodoBeingEdited) (dispatch: Msg -> unit) =
+  div
+    [ "box" ]
+    [ div
+        [ "field"; "is-grouped" ]
+        [ div
+            [ "control"; "is-expanded" ]
+            [ Html.input
+                [ prop.classes [ "input"; "is-medium" ]
+                  prop.valueOrDefault todoBeingEdited.Description
+                  prop.onTextChange (SetEditedDescription >> dispatch) ] ]
+
+          div
+            [ "control"; "buttons" ]
+            [ Html.button
+                [ prop.classes [ "button"; "is-primary" ]
+                  prop.onClick (fun _ -> dispatch ApplyEdit)
+                  prop.children [ Html.i [ prop.classes [ "fa"; "fa-save" ] ] ] ]
+
+              Html.button
+                [ prop.classes [ "button"; "is-warning" ]
+                  prop.onClick (fun _ -> dispatch CancelEdit)
+                  prop.children [ Html.i [ prop.classes [ "fa"; "fa-arrow-right" ] ] ] ] ] ] ]
+
 let renderTodo (todo: Todo) (dispatch: Msg -> unit) =
   div
     [ "box" ]
@@ -101,9 +166,17 @@ let renderTodo (todo: Todo) (dispatch: Msg -> unit) =
             [ div
                 [ "buttons" ]
                 [ Html.button
-                    [ prop.classes [ "button"; if todo.Completed then "is-success"]
+                    [ prop.classes
+                        [ "button"
+                          if todo.Completed then
+                            "is-success" ]
                       prop.onClick (fun _ -> dispatch (ToggleCompleted todo.Id))
                       prop.children [ Html.i [ prop.classes [ "fa"; "fa-check" ] ] ] ]
+
+                  Html.button
+                    [ prop.classes [ "button"; "is-primary" ]
+                      prop.onClick (fun _ -> dispatch (StartEditingTodo todo.Id))
+                      prop.children [ Html.i [ prop.classes [ "fa"; "fa-edit" ] ] ] ]
 
                   Html.button
                     [ prop.classes [ "button"; "is-danger" ]
@@ -111,7 +184,12 @@ let renderTodo (todo: Todo) (dispatch: Msg -> unit) =
                       prop.children [ Html.i [ prop.classes [ "fa"; "fa-times" ] ] ] ] ] ] ] ]
 
 let todoList (state: State) (dispatch: Msg -> unit) =
-  Html.ul [ prop.children [ for todo in state.TodoList -> renderTodo todo dispatch ] ]
+  Html.ul
+    [ prop.children
+        [ for todo in state.TodoList ->
+            match state.TodoBeingEdited with
+            | Some todoBeingEdited when todoBeingEdited.Id = todo.Id -> renderEditForm todoBeingEdited dispatch
+            | _ -> renderTodo todo dispatch ] ]
 
 let render (state: State) (dispatch: Msg -> unit) =
   Html.div
