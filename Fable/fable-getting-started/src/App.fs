f7adbc76d65b840d75f908e85c9189b813e49a70
phasetr <phasetr@gmail.com>
2024-04-18 16:45:31 +0900

    commit

--- Fable/fable-getting-started/src/App.fs
+++ Fable/fable-getting-started/src/App.fs
@@ -46,24 +46,105 @@ let printMsgButton = document.getElementById "printMsg"
 printMsgButton.onclick <- fun _ -> printfn "Button clicked!"
 
 // Elmish
-type State = { Count: int }
+type Validated<'t> = { Raw: string; Parsed: Option<'t> }
+
+module Validated =
+  let createEmpty () : Validated<_> = { Raw = ""; Parsed = None }
+  let success raw value : Validated<_> = { Raw = raw; Parsed = Some value }
+  let failure raw : Validated<_> = { Raw = raw; Parsed = None }
+
+let tryParseInt (input: string) : Validated<int> =
+  try
+    Validated.success input (int input)
+  with _ ->
+    Validated.failure input
+
+let validatedTextColor validated =
+  match validated.Parsed with
+  | Some _ -> color.green
+  | None -> color.crimson
+
+type State =
+  { Count: int
+    TextInput: string
+    NumberInput: Validated<int>
+    Capitalized: bool }
 
 type Msg =
   | Increment
   | Decrement
+  | SetNumberInput of Validated<int>
+  | SetTextInput of string
+  | SetCapitalized of bool
 
-let init () = { Count = 0 }
+let init () =
+  { Count = 0
+    NumberInput = Validated.createEmpty ()
+    TextInput = ""
+    Capitalized = false }
 
 let update (msg: Msg) (state: State) : State =
   match msg with
   | Increment -> { state with Count = state.Count + 1 }
   | Decrement -> { state with Count = state.Count - 1 }
+  | SetNumberInput numberInput -> { state with NumberInput = numberInput }
+  | SetTextInput textInput -> { state with TextInput = textInput }
+  | SetCapitalized value -> { state with Capitalized = value }
 
 let render (state: State) (dispatch: Msg -> unit) =
+  let oddOrEvenMessage =
+    Html.h1
+      [ prop.style
+          [ if state.Count < 0 then
+              style.display.none
+            else
+              style.display.block ]
+        prop.text (
+          if state.Count % 2 = 0 then
+            "Count is even"
+          else
+            "Count is odd"
+        ) ]
+
   Html.div
-    [ Html.button [ prop.onClick (fun _ -> dispatch Increment); prop.text "Increment" ]
-      Html.button [ prop.onClick (fun _ -> dispatch Decrement); prop.text "Decrement" ]
-      Html.h1 state.Count ]
+    [ Html.div
+        [ prop.style [ style.padding 20 ]
+          prop.children
+            [ Html.div
+                [ Html.input
+                    [ prop.valueOrDefault state.TextInput
+                      prop.onChange (SetTextInput >> dispatch) ]
+                  Html.span state.TextInput ]
+
+              Html.div
+                [ Html.label [ prop.htmlFor "checkbox-capitalized"; prop.text "Capitalized" ]
+
+                  Html.input
+                    [ prop.style [ style.margin 5 ]
+                      prop.id "checkbox-capitalized"
+                      prop.type'.checkbox
+                      prop.isChecked state.Capitalized
+                      prop.onChange (SetCapitalized >> dispatch) ]
+
+                  Html.span (
+                    if state.Capitalized then
+                      state.TextInput.ToUpper()
+                    else
+                      state.TextInput
+                  ) ]
+
+              Html.input
+                [ prop.type'.number
+                  prop.valueOrDefault state.NumberInput.Raw
+                  prop.onChange (tryParseInt >> SetNumberInput >> dispatch) ]
+
+              Html.h2
+                [ prop.style [ style.color (validatedTextColor state.NumberInput) ]
+                  prop.text state.NumberInput.Raw ] ] ]
+      Html.button [ prop.onClick (fun _ -> dispatch Increment); prop.text "+" ]
+      Html.button [ prop.onClick (fun _ -> dispatch Decrement); prop.text "-" ]
+      Html.div state.Count
+      oddOrEvenMessage ]
 
 Program.mkSimple init update render
 |> Program.withReactSynchronous "elmish-app"
