fe8cd5930a01029d120c0051a9c7b0f5546bc633
phasetr <phasetr@gmail.com>
2024-04-18 20:38:17 +0900

    commit

---
+++ Fable/Cmd/README.md
@@ -0,0 +1,23 @@
+# Fable Getting Started
+
+- [Original Repository](https://github.com/Zaid-Ajaj/fable-getting-started)
+- [The Elmish book](https://zaid-ajaj.github.io/the-elmish-book/#/)
+- [Elmish Commands](https://zaid-ajaj.github.io/the-elmish-book/#/chapters/commands/commands)から
+
+### 注意
+
+- `Cmd.ofSub`は単に`Cmd.ofEffect`に置き換わった：[参考](https://elmish.github.io/elmish/docs/subscription.html#subscription-reusability)
+
+### 開発用に`watch`で実行
+
+```shell
+npm start
+```
+
+### 単純な実行
+
+- 以下のコマンドを実行して`dist/index.html`をブラウザで開く
+
+```shell
+npm run build
+```
---
+++ Fable/Cmd/src/App.fs
@@ -0,0 +1,89 @@
+module App
+
+open System
+open Elmish
+open Elmish.HMR
+open Fable.Core
+open Feliz
+
+type State =
+  { Count: int
+    Loading: bool
+    Current: DateTime
+    Enabled: bool }
+
+type Msg =
+  | Increment
+  | Decrement
+  | IncrementDelayed
+  | Tick of DateTime
+  | Toggle of Enabled: bool
+
+let init () =
+  { Count = 0
+    Loading = false
+    Current = DateTime.MinValue
+    Enabled = true },
+  Cmd.none
+
+let update msg state =
+  match msg with
+  | Increment ->
+    { state with
+        Count = state.Count + 1
+        Loading = false },
+    Cmd.none
+  | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
+  | IncrementDelayed when state.Loading -> state, Cmd.none
+  | IncrementDelayed ->
+    let incrementDelayedCmd (dispatch: Msg -> unit) : unit =
+      async {
+        do! Async.Sleep 1000
+        dispatch Increment
+      }
+      |> Async.StartImmediate
+
+    { state with Loading = true }, Cmd.ofEffect incrementDelayedCmd
+  | Tick current -> { state with Current = current }, Cmd.none
+  | Toggle enabled -> { state with Enabled = enabled }, Cmd.none
+
+let render (state: State) (dispatch: Msg -> unit) =
+  let timestamp = state.Current.ToString("yyyy-MM-dd HH:mm:ss")
+
+  Html.div
+    [ Html.h1 state.Count
+      Html.button [ prop.onClick (fun _ -> dispatch Increment); prop.text "Increment" ]
+      Html.button [ prop.onClick (fun _ -> dispatch Decrement); prop.text "Decrement" ]
+      Html.button
+        [ prop.disabled state.Loading
+          prop.onClick (fun _ -> dispatch IncrementDelayed)
+          prop.text "Increment Delayed" ]
+      Html.p [ prop.text $"Current time: %A{state.Current}" ]
+      Html.div
+        [ Html.div [ Html.text timestamp ]
+          Html.div
+            [ Html.label
+                [ prop.children
+                    [ Html.input
+                        [ prop.type' "checkbox"
+                          prop.isChecked state.Enabled
+                          prop.onCheckedChange (fun b -> dispatch (Toggle b)) ]
+                      Html.text " enabled" ] ] ] ] ]
+
+let timer _ =
+  let start dispatch =
+    let interValid = JS.setInterval (fun _ -> dispatch (Tick DateTime.Now)) 1000
+
+    { new IDisposable with
+        member _.Dispose() = JS.clearInterval interValid }
+
+  start
+
+let subscribe state =
+  [ if state.Enabled then
+      [ "timer" ], timer Tick ]
+
+Program.mkProgram init update render
+|> Program.withSubscription subscribe
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
