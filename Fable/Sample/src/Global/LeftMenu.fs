beba3a2207e5fc5b0f83c8b652fbddd9eb0d9677
phasetr <phasetr@gmail.com>
2024-05-21 09:48:19 +0900

    commit

--- Fable/Sample/src/Global/LeftMenu.fs
+++ Fable/Sample/src/Global/LeftMenu.fs
@@ -4,22 +4,19 @@ open Feliz
 open Feliz.Router
 open Sample.Global.Svg
 
-let menuData =
-  [ ("/", "Home", svgHome)
-    ("about", "About", svgAbout)
-    ("services", "Services", svgService)
-    ("contact", "Contact", svgContact) ]
-
-let menu =
-  menuData
-  |> List.map (fun (url, text, svg) ->
-    Html.a
-      [ prop.className
-          "flex items-center px-3 py-2 text-gray-600 hover:bg-gray-200 hover:text-gray-900 rounded-md dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-100 font-medium"
-        prop.href url
-        prop.children [ svg; Html.text text ] ])
-
 let leftMenu =
+  let menu =
+    [ ("/", "Home", svgHome)
+      ("about", "About", svgAbout)
+      ("services", "Services", svgService)
+      ("contact", "Contact", svgContact) ]
+    |> List.map (fun (url, text, svg) ->
+      Html.a
+        [ prop.className
+            "flex items-center px-3 py-2 text-gray-600 hover:bg-gray-200 hover:text-gray-900 rounded-md dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-100 font-medium"
+          prop.href (Router.format url)
+          prop.children [ svg; Html.text text ] ])
+
   Html.div
     [ prop.className
         "hidden lg:block bg-gray-100 dark:bg-gray-800 w-64 p-6 border-r border-gray-200 dark:border-gray-700"
@@ -31,14 +28,4 @@ let leftMenu =
                   Html.span
                     [ prop.className "text-lg font-semibold"
                       prop.text "Acme Inc" ] ] ]
-          Html.nav
-            [ prop.className "space-y-1"
-              prop.children (
-                menuData
-                |> List.map (fun (url, text, svg) ->
-                  Html.a
-                    [ prop.className
-                        "flex items-center px-3 py-2 text-gray-600 hover:bg-gray-200 hover:text-gray-900 rounded-md dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-100 font-medium"
-                      prop.href (Router.format url)
-                      prop.children [ svg; Html.text text ] ])
-              ) ] ] ]
+          Html.nav [ prop.className "space-y-1"; prop.children menu ] ] ]
