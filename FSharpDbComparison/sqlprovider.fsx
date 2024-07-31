c4574b685f4d03e2984899dc0fa075f1ecfb9f9e
phasetr <phasetr@gmail.com>
2024-07-31 17:08:58 +0900

    commit

--- FSharpDbComparison/sqlprovider.fsx
+++ FSharpDbComparison/sqlprovider.fsx
@@ -2,23 +2,22 @@
 #r "nuget: SQLProvider"
 
 open FSharp.Data.Sql
-let [<Literal>] connStr = "Data Source=efcore-fsharp.tmp.db"
+let [<Literal>] connStr = "Data Source=efcore-fsharp.tmp.tmp.db"
 let [<Literal>] resolutionPath = "~/.nuget/packages/microsoft.data.sqlite.core/8.0.7/lib/net8.0"
-
 type sql = SqlDataProvider<
-                ConnectionString = connStr,
-                DatabaseVendor = Common.DatabaseProviderTypes.SQLITE,
-                ResolutionPath = resolutionPath,
-                IndividualsAmount = 1000,
-                CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL,
-                UseOptionTypes = FSharp.Data.Sql.Common.NullableColumnType.OPTION>
+  ConnectionString = connStr,
+  ResolutionPath = resolutionPath,
+  DatabaseVendor = Common.DatabaseProviderTypes.SQLITE,
+  IndividualsAmount = 1000,
+  CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL,
+  UseOptionTypes = FSharp.Data.Sql.Common.NullableColumnType.OPTION>
 
 // データベースの操作例
 let ctx = sql.GetDataContext()
 
-let allUsers = query {
-    for user in ctx.Main.Users do
-    select user
-} |> Seq.toList
-
-allUsers |> List.iter (fun user -> printfn "%A" user)
+let allUsers =
+    query {
+        for user in ctx.Main.Users do
+        select user
+    } |> Seq.toList
+allUsers |> List.iter (fun user -> printfn $"%A{user}")
